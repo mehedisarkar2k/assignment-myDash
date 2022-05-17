6fd73948e8b184318e34560814e6f1b9145a0347
Mehedi Sarkar <mehedisarkar2k@gmail.com>
2022-05-17 15:34:08 +0600

    left boc design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import "./App.scss";
+import styles from "./App.module.scss";
 import SignupForm from "./components/SignupForm";
-import bg from "./img/signupFormBg.png";
 
 function App() {
     return (
-        <div style={{ display: "flex" }}>
-            <div className="">
-                <img src={bg} alt="" />
-            </div>
+        <div className={styles.container}>
+            <div className={styles.container__content}>
+                <div className={styles.container__content__leftBox}>
+                    <div className={styles.container__content__leftBox__text}>
+                        <h2>Choose a date range</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Ipsum, quasi nihil?
+                        </p>
+                    </div>
+                </div>
 
-            <SignupForm />
+                <SignupForm />
+            </div>
         </div>
     );
 }
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,3 +1,4 @@
+import styles from "../scss/SignupForm.module.scss";
 import Button from "./Button";
 import Checkbox from "./Checkbox";
 import From from "./Form";
@@ -5,11 +6,23 @@ import TextField from "./TextFiled";
 
 const SignupForm = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Create an account</h1>
 
             <From>
-                <TextField label="Your email address" />
+                <TextField type="email" id="email" label="Your email address" />
+                <TextField
+                    type="password"
+                    id="password"
+                    label="Your password"
+                />
+                <TextField
+                    type="password"
+                    id="password2"
+                    label="Confirm your password"
+                />
+                <TextField type="text" id="name" label="Your full name" />
+                <TextField type="tel" id="phone" label="Your phone number" />
                 <Checkbox label="I read and agree with Terms and Conditions" />
 
                 <Button>Create Account</Button>
